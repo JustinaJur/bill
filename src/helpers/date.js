3d93b92f2baa61c26bb08185fa74b73e25feb9d7
JustinaJur <jurgeleviciute.justina@gmail.com>
2024-12-02 14:28:28 +0200

    add service date

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -9,3 +9,13 @@ export const getCurrrentDate = () => {
   const currentPaddedDate = `${year}-${pMonth}-${pDay}`;
   return currentPaddedDate;
 };
+
+export const getPreviousMonth = () => {
+  const currentDate = new Date();
+  currentDate.setMonth(currentDate.getMonth() - 1);
+
+  const year = currentDate.getUTCFullYear();
+  const month = (currentDate.getUTCMonth() + 1).toString().padStart(2, "0");
+
+  return `${year}-${month}`;
+};
